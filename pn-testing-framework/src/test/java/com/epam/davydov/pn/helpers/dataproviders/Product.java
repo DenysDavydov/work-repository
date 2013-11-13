1ac82630591161ea74597a320fdbee55f9f40865
Denys <big8xxx@gmail.com>
2013-11-13 17:59:08 +0200

    Refactored product page

--- pn-testing-framework/src/test/java/com/epam/davydov/pn/helpers/dataproviders/Product.java
+++ pn-testing-framework/src/test/java/com/epam/davydov/pn/helpers/dataproviders/Product.java
@@ -4,61 +4,59 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Product {
-	public static final String NAME = "название"; 
-    public static final String PRICE = "цена";
-    
-    private Map<String, String> properties;
+	public static final String NAME = "название";
+	public static final String PRICE = "цена";
 
-    public Product() {
-        properties = new HashMap<>();
-    }
+	private Map<String, String> properties;
 
-    public void setProperty(String key, String value) {
-        properties.put(key, value);
-    }
+	public Product() {
+		properties = new HashMap<>();
+	}
 
-    public String getPropertyValue(String key) {
-        return properties.get(key);
-    }
+	public void setProperty(String key, String value) {
+		properties.put(key, value);
+	}
 
-    public int compareItemByProperty(Product product, String key) {
-        return key.equals(PRICE)
-                ? compareProductByPrice(product)
-                : this.getPropertyValue(key).compareTo(product.getPropertyValue(key));
-    }
+	public String getPropertyValue(String key) {
+		return properties.get(key);
+	}
 
-    private int compareProductByPrice(Product product) {
-        int ourPrice = Integer.parseInt(
-                this.getPropertyValue(PRICE).replaceAll("[\\D\\s]", ""));
-        int hisPrice = Integer.parseInt(
-                product.getPropertyValue(PRICE).replaceAll("[\\D\\s]", ""));
-        if (ourPrice > hisPrice)
-            return 1;
-        if (ourPrice < hisPrice)
-            return -1;
-        return 0;
-    }
-    
-    public boolean containsPropertyValue(String propertyValue){
-    	return properties.containsValue(propertyValue);
-    }
+	public int compareItemByProperty(Product product, String key) {
+		return key.equals(PRICE) 
+				? compareProductByPrice(product) 
+				: this.getPropertyValue(key).compareTo(product.getPropertyValue(key));
+	}
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o)
-            return true;
-        if (o == null || getClass() != o.getClass())
-            return false;
+	private int compareProductByPrice(Product product) {
+		int ourPrice = Integer.parseInt(this.getPropertyValue(PRICE).replaceAll("[\\D\\s]", ""));
+		int hisPrice = Integer.parseInt(product.getPropertyValue(PRICE).replaceAll("[\\D\\s]", ""));
+		if (ourPrice > hisPrice)
+			return 1;
+		if (ourPrice < hisPrice)
+			return -1;
+		return 0;
+	}
 
-        Product that = (Product) o;
+	public boolean containsPropertyValue(String propertyValue) {
+		return properties.containsValue(propertyValue);
+	}
 
-        return properties.equals(that.properties);
-    }
+	@Override
+	public boolean equals(Object o) {
+		if (this == o)
+			return true;
+		if (o == null || getClass() != o.getClass())
+			return false;
 
-    @Override
-    public int hashCode() {
-        return properties.hashCode();
-    }
+		Product that = (Product) o;
+
+		return properties.equals(that.properties);
+	}
+
+	@Override
+	public int hashCode() {
+		return properties.hashCode();
+	}
 
 	@Override
 	public String toString() {
